42cc599d3b2821b3d2a8a352bd0f927c31348185
Shapovalov Dmytro <buonviagio@gmail.com>
2023-03-15 18:47:20 +0200

    My first commit Module#6 Task#1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,29 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String fur, puw;
+        if (hasFur) {
+            fur = "a";
+        } else {
+            fur = "no";
+        }
+        if (numberOfPaws == 1) {
+            puw = "puw";
+        } else {
+            puw = "puws";
+        }
+
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + " " + puw + " and " + fur + " fur.";
+    }
 }
